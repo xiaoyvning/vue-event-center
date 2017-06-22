1b6c3404d46d04153cc397d07debd927e31fb55d
fanxingguang <fanxingguang@baidu.com>
2017-06-22 11:38:11 +0800

    去除vue install 使用的话直接import 就行了

--- index.js
+++ index.js
@@ -29,36 +29,32 @@ function handleEvent(isOn) {
         hub[action](name, events[name]);
     }
 }
-export default {
-    install(Vue) {
-        // 全局注册一个混合，所有Vue对象在初始化的时候检测$options中的events
-        // 然后created(创建)时在eventHub中注册事件，在组件beforeDestroy(注销前)解绑事件
-        Vue.mixin({
-            created() {
-                if (!this.$options.events) {
-                    return;
-                }
+// 全局注册一个混合，所有Vue对象在初始化的时候检测$options中的events
+// 然后created(创建)时在eventHub中注册事件，在组件beforeDestroy(注销前)解绑事件
+Vue.mixin({
+    created() {
+        if (!this.$options.events) {
+            return;
+        }
 
-                let events = this.$options.events;
-                this.$options._events = {};
-                for (let name of Object.keys(events)) {
-                    this.$options._events[name] = events[name].bind(this);
-                }
+        let events = this.$options.events;
+        this.$options._events = {};
+        for (let name of Object.keys(events)) {
+            this.$options._events[name] = events[name].bind(this);
+        }
 
-                handleEvent.call(this, true);
-            },
-            beforeDestroy() {
-                handleEvent.call(this, false);
-            }
-        });
-
-        /**
-         * Vue全局触发事件
-         *
-         * @param {...*} args [参数列表]
-         */
-        Vue.prototype.$trigger = function (...args) {
-            hub.$emit.apply(hub, args);
-        };
+        handleEvent.call(this, true);
+    },
+    beforeDestroy() {
+        handleEvent.call(this, false);
     }
+});
+
+/**
+ * Vue全局触发事件
+ *
+ * @param {...*} args [参数列表]
+ */
+Vue.prototype.$trigger = function (...args) {
+    hub.$emit.apply(hub, args);
 };
